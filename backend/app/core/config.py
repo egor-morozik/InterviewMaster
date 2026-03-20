8d216abf0cfed0adec9f6a1fc63e0e4274ddf6ef
egor-morozik <egor.mrz04@gmail.com>
2026-03-20 23:30:59 +0300

    feature: use pgvector & add alembic with migrations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 class Settings:
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "user")
@@ -7,10 +12,10 @@ class Settings:
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "localhost")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "interview_db")
-    POSTGRES_URL: str = os.getenv(
-        "POSTGRES_URL",
-        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
-    )
+
+    @property
+    def POSTGRES_URL(self):
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 settings = Settings()
